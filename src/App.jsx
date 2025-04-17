e63477bbf08a7b2dfaae1af65b4334f046c25a09
Ali ahmed <aa5325368@gmail.com>
2025-04-17 12:48:31 +0200

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Committees from './pages/Committees'
 import Workshops from './pages/Workshops'
 import Events from './pages/Events'
 import OurTeams from './pages/OurTeams'
+import Contact from './pages/Contact'
 
 function App() {
   
@@ -30,6 +31,8 @@ function App() {
           <Route path='/events' element={<Events />} />
 
           <Route path='/teams' element={<OurTeams />} />
+
+          <Route path='/contact' element={<Contact />} />
 
 
         </Routes>
